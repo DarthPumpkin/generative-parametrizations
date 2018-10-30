fc58d76d8ccef933f90f2d83827da9a3bee88df1
Carlo Rapisarda <carlora@kth.se>
2018-10-30 13:38:09 +0100

    Refactor tests for unittest

--- tests/reward_functions.py
+++ tests/reward_functions.py
@@ -1,5 +1,6 @@
 import gym
 import numpy as np
+import unittest
 from gym.envs.classic_control import PendulumEnv
 from gym.envs.robotics import FetchEnv
 
@@ -7,81 +8,75 @@ import _gpp
 from gpp.reward_functions import RewardFunction
 
 
-def test_pendulum_env():
+class TestRewardFunction(unittest.TestCase):
 
-    env = gym.make('Pendulum-v0')
-    raw_env = env.unwrapped # type: PendulumEnv
-    reward_fn = RewardFunction(env)
+    def test_pendulum_env(self):
 
-    obs = env.reset()
-    n_trials = 10000
-
-    rewards_a = np.zeros(n_trials)
-    all_actions = np.zeros((n_trials,) + raw_env.action_space.shape)
-    all_obs = np.zeros((n_trials,) + raw_env.observation_space.shape)
-
-    for i in range(n_trials):
-        action = raw_env.action_space.sample()
-        all_obs[i] = obs.copy()
-        all_actions[i] = action.copy()
-
-        reward_b = reward_fn(obs, actions=action)
-        obs, reward_a, _, _ = env.step(action)
-        rewards_a[i] = reward_a
-        assert np.allclose(reward_a, reward_b)
-
-    rewards_b = reward_fn(all_obs, actions=all_actions)
-    assert np.allclose(rewards_a, rewards_b)
-
-    print('==> test_pendulum_env PASSED')
-
-
-def test_fetch_env():
-
-    cases = [
-        'FetchReach-v1',
-        'FetchSlideDense-v1',
-        'FetchPickAndPlaceSphere-v1',
-        'FetchPush-v1'
-    ]
-
-    for c in cases:
-
-        env = gym.make(c)
-        raw_env = env.unwrapped  # type: FetchEnv
+        env = gym.make('Pendulum-v0')
+        raw_env = env.unwrapped # type: PendulumEnv
         reward_fn = RewardFunction(env)
 
-        env.reset()
-        n_trials = 2000
-
-        obs_space_shape = raw_env.observation_space.spaces['observation'].shape
+        obs = env.reset()
+        n_trials = 10000
 
         rewards_a = np.zeros(n_trials)
         all_actions = np.zeros((n_trials,) + raw_env.action_space.shape)
-        all_obs = np.zeros((n_trials,) + obs_space_shape)
-        goal = None
+        all_obs = np.zeros((n_trials,) + raw_env.observation_space.shape)
 
         for i in range(n_trials):
             action = raw_env.action_space.sample()
-            obs, reward_a, _, _ = env.step(action)
-
-            goal = obs['desired_goal']
-            obs = obs['observation']
             all_obs[i] = obs.copy()
             all_actions[i] = action.copy()
 
-            reward_b = reward_fn(obs, goal=goal)
+            reward_b = reward_fn(obs, actions=action)
+            obs, reward_a, _, _ = env.step(action)
             rewards_a[i] = reward_a
-            assert np.allclose(reward_a, reward_b)
+            self.assertTrue(np.allclose(reward_a, reward_b))
 
-        rewards_b = reward_fn(all_obs, goal=goal)
-        assert np.allclose(rewards_a, rewards_b)
-        print(f'==> test_fetch_env case {c} PASSED')
+        rewards_b = reward_fn(all_obs, actions=all_actions)
+        self.assertTrue(np.allclose(rewards_a, rewards_b))
 
-    print('==> test_fetch_env PASSED')
+    def test_fetch_env(self):
+
+        cases = [
+            'FetchReach-v1',
+            'FetchSlideDense-v1',
+            'FetchPickAndPlaceSphere-v1',
+            'FetchPush-v1'
+        ]
+
+        for c in cases:
+
+            env = gym.make(c)
+            raw_env = env.unwrapped  # type: FetchEnv
+            reward_fn = RewardFunction(env)
+
+            env.reset()
+            n_trials = 2000
+
+            obs_space_shape = raw_env.observation_space.spaces['observation'].shape
+
+            rewards_a = np.zeros(n_trials)
+            all_actions = np.zeros((n_trials,) + raw_env.action_space.shape)
+            all_obs = np.zeros((n_trials,) + obs_space_shape)
+            goal = None
+
+            for i in range(n_trials):
+                action = raw_env.action_space.sample()
+                obs, reward_a, _, _ = env.step(action)
+
+                goal = obs['desired_goal']
+                obs = obs['observation']
+                all_obs[i] = obs.copy()
+                all_actions[i] = action.copy()
+
+                reward_b = reward_fn(obs, goal=goal)
+                rewards_a[i] = reward_a
+                self.assertTrue(np.allclose(reward_a, reward_b))
+
+            rewards_b = reward_fn(all_obs, goal=goal)
+            self.assertTrue(np.allclose(rewards_a, rewards_b))
 
 
 if __name__ == '__main__':
-    test_pendulum_env()
-    test_fetch_env()
-    print('==> all tests PASSED')
+    unittest.main(verbosity=2)
