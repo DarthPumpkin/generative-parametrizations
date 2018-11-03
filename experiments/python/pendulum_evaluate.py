432253b1155b37a62e37a3fc48aedb1d0ac2dc2f
Carlo Rapisarda <carlora@kth.se>
2018-11-03 17:59:02 +0100

    Fix results dataframe, enable TRPO when evaluating models on pendulum

--- experiments/python/pendulum_evaluate.py
+++ experiments/python/pendulum_evaluate.py
@@ -3,6 +3,7 @@ from timeit import default_timer as timer
 
 import numpy as np
 from joblib import Parallel, delayed
+from stable_baselines import TRPO
 import gym
 import pandas as pd
 # noinspection PyUnresolvedReferences
@@ -14,28 +15,50 @@ from gpp.mpc import MPC
 TEST_RUNS = 50
 MASS_MEANS = [1]
 MASS_STDS = [0.5]
+MODEL_TYPE = 'mdn'
 
-MODEL_PATH = "../out/tmp_mdn_model.pkl"
-SAVE_PATH = "../out/tmp_mdn_model.csv"
+if MODEL_TYPE == 'trpo':
+    MODEL_PATH = "../out/tmp_trpo_model.pkl"
+    SAVE_PATH = "../out/tmp_trpo_model.csv"
+elif MODEL_TYPE == 'mdn':
+    MODEL_PATH = "../out/tmp_mdn_model.pkl"
+    SAVE_PATH = "../out/tmp_mdn_model.csv"
 
 
 def _evaluation_worker(seed, runs):
 
+    model_type = MODEL_TYPE
+
     # init environment
     env = gym.make('GaussianPendulum-v0')
 
-    # load model
-    mdn = MDN_Model.load(Path(MODEL_PATH))
-    mpc = MPC(env, mdn, horizon=20, n_action_sequences=2000, np_random=None)
+    if model_type == 'mdn':
 
-    def next_mdn_mpc_action(obs):
-        return mpc.get_action(obs)
+        # load model
+        mdn = MDN_Model.load(Path(MODEL_PATH))
+        mpc = MPC(env, mdn, horizon=20, n_action_sequences=2000, np_random=None)
 
-    results = []
+        def next_action(obs):
+            return mpc.get_action(obs)
+
+    elif model_type == 'trpo':
+
+        # load model
+        model = TRPO.load(MODEL_PATH, env=env)
+
+        def next_action(obs):
+            action, _ = model.predict(obs, deterministic=True)
+            return action
+
+    else:
+        raise NotImplementedError
+
+    results = pd.DataFrame()
     for mean, std in zip(MASS_MEANS, MASS_STDS):
-        masses, rewards = run_model_on_pendulum.test(env, next_mdn_mpc_action, runs, mean, std, seed=seed)
+        masses, rewards = run_model_on_pendulum.test(env, next_action, runs, mean, std, seed=seed)
         t, = masses.shape
-        results.append({"mass": masses, "mass_mean": [mean] * t, "mass_std": [std] * t, "reward": rewards})
+        df = pd.DataFrame({"mass": masses, "mass_mean": [mean] * t, "mass_std": [std] * t, "reward": rewards})
+        results = results.append(df)
 
     return results
 
@@ -50,15 +73,15 @@ def run_evaluation(workers=1, seed=42):
         runs_per_worker = np.ones(workers).astype(np.int) * (TEST_RUNS // workers)
         runs_per_worker[-1] += TEST_RUNS % workers
         results = parallel(delayed(_evaluation_worker)(seed + i, runs_per_worker[i]) for i in range(workers))
-        results = sum(results, [])
+        results = pd.concat(results)
     else:
         results = _evaluation_worker(seed, TEST_RUNS)
 
     tock_t = timer()
-    print("Done. Took ~{}s".format(round(tock_t - tick_t)))
+    print(f'Done. Took ~{round(tock_t - tick_t)}s')
+    print(f'Mean reward = {results.mean()}')
 
-    df = pd.DataFrame(results)
-    df.to_csv(SAVE_PATH)
+    results.to_csv(SAVE_PATH)
 
 
 if __name__ == '__main__':
