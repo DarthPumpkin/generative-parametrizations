308630756c6f9f7e769c0b5afb44e01a6fcd05c9
Carlo Rapisarda <carlora@kth.se>
2018-11-03 16:02:30 +0100

    Remove forgotten exception

--- gpp/reward_functions.py
+++ gpp/reward_functions.py
@@ -4,7 +4,6 @@ import numpy as np
 
 try:
     from gym.envs.robotics import FetchEnv
-    raise gym.error.DependencyNotInstalled
 except (ImportError, gym.error.DependencyNotInstalled) as e:
     FetchEnv = None
     print('WARNING: Could not import robotics environments! MuJoCo might not be installed.')
