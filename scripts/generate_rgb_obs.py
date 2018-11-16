c8e29b37c2b1afed63a9530d5e8cfca0c6c6a6d5
Carlo Rapisarda <carlora@kth.se>
2018-11-16 14:54:32 +0100

    Improve script to generate images of an arbitrary environment

--- scripts/generate_rgb_obs.py
+++ scripts/generate_rgb_obs.py
@@ -1,31 +1,56 @@
 from cv2 import resize, INTER_AREA
-
 import numpy as np
 import gym
-from gym.envs.classic_control import PendulumEnv
+
+CONFIGS = dict(
+    pendulum=dict(
+        env='Pendulum-v0',
+        size=(256, 256),
+        episodes=200,
+        episode_length=5
+    ),
+    fetch_sphere=dict(
+        env='FetchPickAndPlaceSphere-v1',
+        size=(256, 256),
+        episodes=200,
+        episode_length=5,
+        rgb_options=dict(camera_id=3)
+    )
+)
 
 
-def generate(episodes=200, episode_length=5):
+def generate(config_name: str):
 
-    env = gym.make('Pendulum-v0')
-    raw_env = env.unwrapped  # type: PendulumEnv
+    config = CONFIGS[config_name]
+    episodes = config.get('episodes', 200)
+    episode_length = config.get('episode_length', 5)
+    img_size = config.get('size', (256, 256))
+    rgb_options = config.get('rgb_options', None)
+
+    env = gym.make(config['env'])
+    raw_env = env.unwrapped
+
+    render_kwargs = dict(mode='rgb_array')
+    if rgb_options is not None:
+        render_kwargs['rgb_options'] = rgb_options
 
     data = []
 
     for e in range(episodes):
         env.reset()
         for s in range(episode_length):
-            img = env.render(mode='rgb_array')
-            img = resize(img, dsize=(256, 256), interpolation=INTER_AREA)
+            img = env.render(**render_kwargs)
+            img = resize(img, dsize=img_size, interpolation=INTER_AREA)
             data.append(img)
             rand_action = raw_env.action_space.sample()
             env.step(rand_action)
 
     data = np.array(data)
-    np.save('../data/pendulum_imgs', data)
+    np.savez_compressed(f'../data/{config_name}_imgs', data)
 
     print('Done.')
 
 
 if __name__ == '__main__':
-    generate()
+    for k in CONFIGS.keys():
+        generate(k)
