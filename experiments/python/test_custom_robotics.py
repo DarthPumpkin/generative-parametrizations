3d5d7a352b95760632a42f2ddb9fc3e2916bc3dd
Carlo Rapisarda <carlora@kth.se>
2018-11-17 17:11:04 +0100

    Test methods to resize sites and hide mocap bodies in robotics envs

--- experiments/python/test_custom_robotics.py
+++ experiments/python/test_custom_robotics.py
@@ -2,15 +2,32 @@ import gym
 import time
 import numpy as np
 import mujoco_py as mj
+from gym.envs.robotics import FetchPickAndPlaceSphereEnv
 
 
 if __name__ == '__main__':
 
     env = gym.make("FetchPickAndPlaceSphere-v1")
-    sim = env.env.sim # type: mj.MjSim
+    raw_env = env.unwrapped # type: FetchPickAndPlaceSphereEnv
+    sim = raw_env.sim # type: mj.MjSim
+
+    raw_env.target_in_the_air = False
+    raw_env.mocap_bodies_visible = False
 
     sim.model.opt.gravity[0] = 2.0
     sim.model.opt.gravity[1] = 0.2
+
+    object_name = 'object0'
+    target_name = 'target0'
+
+    object_site_i = sim.model.site_names.index(object_name)
+    target_site_i = sim.model.site_names.index(target_name)
+
+    sim.model.site_size[object_site_i] *= 1.5 * 2.0
+    sim.model.site_size[target_site_i] *= 1.5
+
+    # this doesn't seem to work sadly!
+    # sim.model.geom_size[23] *= 2.5
 
     observation = env.reset()
     while True:
