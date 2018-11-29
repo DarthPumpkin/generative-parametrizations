3a3bd9096f18a89dec72a6e27ed43522e9f842bf
Carlo Rapisarda <carlora@kth.se>
2018-11-29 11:28:55 +0100

    Add images of robotics env w/ longer episodes & changing colors (64x64!)

--- scripts/generate_rgb_obs.py
+++ scripts/generate_rgb_obs.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from tqdm import tqdm
 from cv2 import resize, INTER_AREA
 import numpy as np
 import gym
@@ -14,6 +15,22 @@ def _setup_fetch_sphere_big(env):
     target_site_i = model.site_names.index('target0')
     model.site_size[object_site_i] *= 1.5 * 2.0
     model.site_size[target_site_i] *= 1.5
+
+
+def _setup_fetch_sphere_big_longer(env):
+    raw_env = env.unwrapped
+    raw_env.block_gripper = True
+    _setup_fetch_sphere_big(env)
+
+
+def _reset_fetch_sphere_big_longer_color(env):
+    raw_env = env.unwrapped
+    model = raw_env.sim.model
+    object_site_i = model.site_names.index('object0')
+    yellow = (1.0, 1.0, 0.0, 1.0)
+    blue = (0.0, 0.0, 1.0, 1.0)
+    random_i = np.random.choice(2)
+    model.site_rgba[object_site_i] = [yellow, blue][random_i]
 
 
 def _reset_pendulum_var_length(env):
@@ -46,6 +63,25 @@ CONFIGS = dict(
         rgb_options=dict(camera_id=3),
         env_setup=_setup_fetch_sphere_big
     ),
+    fetch_sphere_big_longer=dict(
+        env='FetchPickAndPlaceSphere-v1',
+        size=(64, 64),
+        episodes=200,
+        episode_length=32,
+        rgb_options=dict(camera_id=3),
+        action_period=6,
+        env_setup=_setup_fetch_sphere_big_longer
+    ),
+    fetch_sphere_big_longer_color=dict(
+        env='FetchPickAndPlaceSphere-v1',
+        size=(64, 64),
+        episodes=200,
+        episode_length=32,
+        rgb_options=dict(camera_id=3),
+        action_period=6,
+        env_setup=_setup_fetch_sphere_big_longer,
+        env_reset=_reset_fetch_sphere_big_longer_color
+    ),
     pendulum_var_length=dict(
         env='Pendulum-v0',
         size=(256, 256),
@@ -65,6 +101,7 @@ def generate(config_name: str):
     rgb_options = config.get('rgb_options', None)
     env_setup = config.get('env_setup', None)
     env_reset = config.get('env_reset', None)
+    action_period = config.get('action_period', 1)
 
     file_path = f'../data/{config_name}_imgs.npz'
     if Path(file_path).exists():
@@ -81,25 +118,55 @@ def generate(config_name: str):
     if callable(env_setup):
         env_setup(env)
 
+    print(f'Generating {config_name}...')
     data = []
-
-    for e in range(episodes):
+    for e in tqdm(range(episodes)):
         env.reset()
         if callable(env_reset):
             env_reset(env)
         for s in range(episode_length):
+            if s % action_period == 0:
+                rand_action = raw_env.action_space.sample()
             img = env.render(**render_kwargs)
             img = resize(img, dsize=img_size, interpolation=INTER_AREA)
             data.append(img)
-            rand_action = raw_env.action_space.sample()
             env.step(rand_action)
 
     data = np.array(data)
     np.savez_compressed(file_path, data)
+    print('Done.')
 
+
+def crop(config_name: str, size):
+
+    if isinstance(size, int):
+        size = (size, size)
+    assert len(size) == 2, 'size must be a tuple of two ints'
+    assert isinstance(size[0], int) and isinstance(size[1], int), 'size must be a tuple of two ints'
+
+    file_path = f'../data/{config_name}_imgs.npz'
+    if not Path(file_path).exists():
+        print(f'Skipping {config_name} as it doesn\'t exist.')
+        return
+
+    data = np.load(file_path)['arr_0']
+    if data.shape[1:3] == size:
+        print(f'Skipping {config_name} as it is already at the desired size.')
+        return
+
+    assert data.shape[1] > size[0] and data.shape[2] > size[1], 'size must be smaller than original'
+
+    print(f'Cropping {config_name}...')
+    cropped = np.zeros((data.shape[0],) + size + data.shape[3:], dtype=data.dtype)
+    for i, img in enumerate(data):
+        cropped[i] = resize(img, dsize=size, interpolation=INTER_AREA)
+
+    np.savez_compressed(file_path, cropped)
     print('Done.')
 
 
 if __name__ == '__main__':
     for k in CONFIGS.keys():
         generate(k)
+    crop('fetch_sphere_big_longer', 64)
+    crop('fetch_sphere_big_longer_color', 64)
