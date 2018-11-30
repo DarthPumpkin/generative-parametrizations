bfc44fd667800bf7067adb1ba7850dcdf30afaac
Carlo Rapisarda <carlora@kth.se>
2018-11-30 17:16:37 +0100

    Add strategy parameter when generating the dataset

--- gpp/dataset.py
+++ gpp/dataset.py
@@ -1,6 +1,7 @@
 import pickle
 from pathlib import Path
 
+from tqdm import tqdm
 import gym
 from torch.utils.data import Dataset
 from .models.utilities import get_observation_space, get_observations
@@ -41,21 +42,27 @@ class EnvDataset(Dataset):
             return 0
         return self.data[0][0][0].shape
 
-    def generate(self, episodes: int, episode_length: int):
+    def generate(self, episodes: int, episode_length: int, strategy=None):
         env, raw_env = self.env, self._raw_env
         observation_space = get_observation_space(env)
+
+        def rand_strategy(*args):
+            return raw_env.action_space.sample()
+
+        strategy = strategy or rand_strategy
         data = []
-        for e in range(episodes):
+        for e in tqdm(range(episodes)):
             env.reset()
             obs = get_observations(env)
             actions = np.zeros((episode_length,) + raw_env.action_space.shape)
             states = np.zeros((episode_length + 1,) + observation_space.shape)
             states[0] = obs
             for s in range(episode_length):
-                rand_action = raw_env.action_space.sample()
-                _, rewards, dones, info = env.step(rand_action)
-                states[s + 1] = get_observations(env)
-                actions[s] = rand_action
+                a = strategy(raw_env, obs)
+                _, rewards, dones, info = env.step(a)
+                obs = get_observations(env)
+                states[s + 1] = obs
+                actions[s] = a
             data.append((states, actions))
         self.data = data
         return data
