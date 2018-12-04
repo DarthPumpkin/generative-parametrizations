517430a25d3f5e461de62d7fa18368f7919298e4
Carlo Rapisarda <carlora@kth.se>
2018-12-05 00:12:48 +0100

    Add basic skeleton for vision-based mpc-compatible model wrappers

--- gpp/models/__init__.py
+++ gpp/models/__init__.py
@@ -2,4 +2,5 @@ from .base import BaseModel
 from .dummy import DummyModel
 from .mdn_model import MDN_Model
 from .lstm_model import LSTM_Model
+from .vision import VisionModel, VaeLstmModel
 from .simulator import PendulumSim, CartPoleSim, RoboticsSim
