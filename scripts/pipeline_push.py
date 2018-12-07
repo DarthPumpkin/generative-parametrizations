86e0da9ca7df9b02f7b08f7516a1ca61ab4dc5ba
Carlo Rapisarda <carlora@kth.se>
2018-12-07 19:09:11 +0100

    Update pipeline for push env

--- scripts/pipeline_push.py
+++ scripts/pipeline_push.py
@@ -9,14 +9,18 @@ from gym.envs.classic_control import GaussianPendulumEnv
 import _gpp
 from gpp.mpc import MPC
 from gpp.models.vision import VaeLstmTFModel
+from reward_functions import _build_simplified_reward_fn_push_env
+from generate_rgb_obs import _reset_push_sphere_v0
 
-
+l2l_model_path = Path('./out/push_sphere_v1_l2lmodel.hdf5')
+# l2reward_model_path = Path('./out/l2rewardmode-reward_exp2_coeff1.h5')
+l2reward_model_path = Path('./out/l2rewardmodel.h5')
 VAE_MODEL_PATH = Path('./tf_vae/kl2rl1-z16-b250-push_sphere_v0vae-fetch199.json')
 
 VAE_ARGS = dict(z_size=16, batch_size=32)
 
-MPC_HORIZON = 10
-MPC_SEQUENCES = 10_000
+MPC_HORIZON = 4
+MPC_SEQUENCES = 5_000
 
 
 def _setup_fetch_sphere_big(env):
@@ -46,20 +50,30 @@ def main():
 
     _setup_fetch_sphere_big_longer(env)
 
-    model = VaeLstmTFModel(VAE_MODEL_PATH, **VAE_ARGS)
+    reward_fn = _build_simplified_reward_fn_push_env(coeff=0.0, exp=1.0)
+
+    model = VaeLstmTFModel(VAE_MODEL_PATH, l2l_model_path=l2l_model_path,
+                           l2reward_model_path=l2reward_model_path, **VAE_ARGS)
     controller = MPC(env, model, MPC_HORIZON, MPC_SEQUENCES,
                      np_random, use_history=True, direct_reward=True,
-                     action_period=10)
+                     action_period=1)
 
     for e in range(2000):
-        env.reset()
+        env_obs = env.reset()
+        _reset_push_sphere_v0(env)
         controller.forget_history()
         for s in range(100):
             rgb_obs = env.render(mode='rgb_array', rgb_options=dict(camera_id=3))
             action = controller.get_action(rgb_obs)
-            _, rewards, dones, info = env.step(action)
-            print(f'Actual reward: {rewards}')
-            sleep(1. / 60)
+
+            env_obs, rewards, dones, info = env.step(action)
+
+            raw_obs = env_obs['observation']
+            goal = env_obs['desired_goal']
+            mod_rew = reward_fn(raw_obs, goal=goal).item()
+
+            print(f'Actual reward: {mod_rew}')
+            # sleep(1. / 60)
 
 
 if __name__ == '__main__':
